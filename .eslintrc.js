e57dd38e9f53bcacff7d776e42db73a38f2daf7e
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-12-19 17:50:12 +0900

    eslint prettier 設定の更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,34 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es2021: true
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
   ],
-  plugins: [
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'space-before-function-paren': 0,
+    'no-console': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ],
+    'vue/singleline-html-element-content-newline': [
+      'error',
+      {
+        ignores: ['NuxtLink']
+      }
+    ]
+  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,44 +4,45 @@
     <h2>↓googleでログイン↓</h2>
     <button @click="googleLogin">ログイン</button>
     <hr />
-    <input type="text" v-model="memo.text" />
+    <input v-model="memo.text" type="text" />
     <button @click="addMemo">追加</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "@nuxtjs/composition-api";
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   setup(_, { root }) {
     /**
      * ログイン機能
      */
+    // eslint-disable-next-line require-await
     const googleLogin = async () => {
       try {
-        const provider = new root.$fireModule.auth.GoogleAuthProvider();
-        root.$fire.auth.signInWithRedirect(provider);
+        const provider = new root.$fireModule.auth.GoogleAuthProvider()
+        root.$fire.auth.signInWithRedirect(provider)
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
-    };
+    }
 
     const memo = reactive({
-      text: null,
-    });
+      text: null
+    })
 
     const addMemo = async () => {
       try {
-        await root.$fire.firestore.collection("memos").add({
-          text: memo.text,
-        });
-        memo.text = null;
+        await root.$fire.firestore.collection('memos').add({
+          text: memo.text
+        })
+        memo.text = null
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
-    };
+    }
 
-    return { googleLogin, memo, addMemo };
-  },
-});
+    return { googleLogin, memo, addMemo }
+  }
+})
 </script>
